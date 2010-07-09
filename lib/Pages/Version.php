8f74565c8060b27df03939dd0687c9bfb4abb9bf
Drak <drak@zikula.org>
2010-07-09 15:45:25 +0000

    Correct version.php

--- lib/Pages/Version.php
+++ lib/Pages/Version.php
@@ -12,10 +12,10 @@ class Pages_Version extends Zikula_Version
     public function getMetaData()
     {
         $meta = array();
-        $meta['displayname']    = __('Static pages', $dom);
-        $meta['description']    = __('Manager of the static pages of the site.', $dom);
+        $meta['displayname']    = $this->__('Static pages');
+        $meta['description']    = $this->__('Manager of the static pages of the site.');
         //! this defines the module's url
-        $meta['url']            = __('pages', $dom);
+        $meta['url']            = $this->__('pages');
         $meta['version']        = '2.4.2';
         $meta['contact']        = 'http://zikula.org/';
 
