79e5628a8e6cd4adee7110dbb3b626ee997e4b42
Craig Heydenburg <craigh@mac.com>
2015-06-24 20:06:09 -0400

    implement new method of link collection for COre-2.0 refs https://github.com/zikula/core/commit/d079001834ed4ca5697c1154c4e36815236c636a

---
+++ Container/LinkContainer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright Pages Team 2015
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Pages
+ * @link https://github.com/zikula-modules/Pages
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\PagesModule\Container;
+
+use Symfony\Component\Routing\RouterInterface;
+use Zikula\Common\Translator\Translator;
+use Zikula\Core\LinkContainer\LinkContainerInterface;
+
+class LinkContainer implements LinkContainerInterface
+{
+    /**
+     * @var Translator
+     */
+    private $translator;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct($translator, RouterInterface $router)
+    {
+        $this->translator = $translator;
+        $this->router = $router;
+    }
+
+    public function getLinks($type = LinkContainerInterface::TYPE_ADMIN)
+    {
+        $links = array();
+        if (\SecurityUtil::checkPermission('ZikulaPagesModule::', '::', ACCESS_READ)) {
+            $links[] = array(
+                'url' => $this->router->generate('zikulapagesmodule_admin_index'),
+                'text' => $this->translator->__('Pages list'),
+                'icon' => 'list');
+        }
+        if (\SecurityUtil::checkPermission('ZikulaPagesModule::', '::', ACCESS_ADD)) {
+            $links[] = array(
+                'url' => $this->router->generate('zikulapagesmodule_adminform_edit'),
+                'text' => $this->translator->__('New Page'),
+                'icon' => 'plus');
+        }
+        if (\SecurityUtil::checkPermission('ZikulaPagesModule::', '::', ACCESS_ADMIN)) {
+            $links[] = array(
+                'url' => $this->router->generate('zikulapagesmodule_admin_purge'),
+                'text' => $this->translator->__('Purge permalinks'),
+                'icon' => 'refresh');
+            $links[] = array(
+                'url' => $this->router->generate('zikulapagesmodule_adminform_config'),
+                'text' => $this->translator->__('Modify Config'),
+                'icon' => 'wrench');
+        }
+
+        return $links;
+    }
+
+    public function getBundleName()
+    {
+        return 'ZikulaPagesModule';
+    }
+}
