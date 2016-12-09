a2e07263e73aea8f884d83187215a059b84635b9
Craig Heydenburg <craigh@mac.com>
2016-12-09 17:19:48 -0500

    update copyright header

--- AdminAuthInterface.php
+++ AdminAuthInterface.php
@@ -1,9 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: craig
- * Date: 5/26/15
- * Time: 6:04 PM
+
+/*
+ * This file is part of the ZikulaPagesModule package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Zikula\PagesModule;
